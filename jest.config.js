bd70e86196733943138801f3fbe048b2f4d8670f
jawash5 <857723555@qq.com>
2021-07-25 21:26:24 +0800

    init: 支持vue测试

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   preset: "ts-jest", // Jest 配置基础的预设
   testEnvironment: 'jest-environment-jsdom-fifteen', // 测试环境。Jest 中的默认环境是 Node.js 环境。如果正在构建 Web 应用程序，则可以改用类似浏览器的环境jsdom。此处使用jsdom15，需安装相应依赖
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"], // 文件扩展名数组
+  transform: {
+    "^.+\\.(vue|md)$": "<rootDir>/node_modules/vue-jest"
+  },  // 用 `vue-jest` 处理 `*.vue` 文件
   globals: {
     "ts-jest": {
       babelConfig: true // 启用 Babel 处理，使用 babel.config.js
--- src/test/demo.test.ts
+++ src/test/demo.test.ts
@@ -1,5 +1,18 @@
 import {add} from "./utils";
+import HelloWorld from "../components/HelloWorld.vue";
+import {mount} from "@vue/test-utils";
 
-test('add', () => {
-  expect(add(1,2)).toBe(3)
+describe('demo', () => {
+  it('add', () => {
+    expect(add(1,2)).toBe(3)
+  })
+
+  it('组件测试', function () {
+    const wrapper = mount(HelloWorld, {
+      props: {
+        msg: 'Hello Vue 3 + TypeScript + Vite'
+      }
+    })
+    expect(wrapper.get("h1").html()).toBe('<h1>Hello Vue 3 + TypeScript + Vite</h1>')
+  });
 })
